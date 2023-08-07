8b0ca8bdca5927acfdcbc2b76efb95c0d672a3c4
daniash84 <danielle.hess84@gmail.com>
2023-08-07 15:41:02 -0700

    js update

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,29 @@
-var weatherApi ="7a3f1c9278ef6f46f295aefa9869a57b"
+var APIkey ="7a3f1c9278ef6f46f295aefa9869a57b"
 var city;
 var country;
+var lat;
+var lon;
+var geo = "http://api.openweathermap.org/geo/1.0/direct?q= " + city +"&appid="  + APIkey;
+var fiveDay= []
+var queryURL = "api.openweathermap.org/data/2.5/forecast?lat= " + lat + "&lon=" + lon + "&appid=" + APIkey;
+// "getButton" 
+// function getCityLocation() {
+// getButton value (city = "getButton" value) 
+// fetch(geo)
+// .then(function (response) {
+    //   return response.json();
+    // }) .then (function(data){})
+    // function getWeather ()
+    // fetch (queryURL)
+    // .then(function (response){})
+    // for (var i=0); i < weatherLength; i+8){}
+// }
+// fetch 
+
+// textContent 
+// localStorage  array is needed get value add value to storage
 
 
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
